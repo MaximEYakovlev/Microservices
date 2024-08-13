82174e11c30bafbac8b1e0cbfcf60636410ba388
Maxim Yakovlev <it.trainee@yandex.ru>
2024-08-13 16:58:49 +0300

    configured postgresql to run in docker container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Pool } from 'pg';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'DATABASE_POOL',
+      useFactory: async () => {
+        const pool = new Pool({
+          host: process.env.DATABASE_HOST,
+          port: parseInt(process.env.DATABASE_PORT, 10),
+          user: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+        });
+        await pool.query('SELECT 1');
+        console.log('Connected to the database successfully!');
+
+        return pool;
+      },
+    },
+  ],
+  exports: ['DATABASE_POOL'],
 })
 export class AppModule {}
